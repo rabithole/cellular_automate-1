25f06aee37963bf2cdbc637cc51277ac28f7af28
Robert Kulp <pedalinon@gmail.com>
2020-08-20 21:32:08 -0700

    Progress of understanding

--- cellular-automata/src/components/Canvas.js
+++ cellular-automata/src/components/Canvas.js
@@ -10,18 +10,20 @@ function Canvas(props) {
 		console.log('draw')
 
 		const canvas = document.getElementById('canvas');
-		console.log(canvas)
+		let ch = 700;
+		let cw = 700;
+		canvas.height = ch;
+		canvas.width = cw;
 
-		if (canvas.getContext) {
-			x = 100
+		// if (canvas.getContext) {
 			const ctx = canvas.getContext('2d');
 
 			ctx.fillStyle = 'rgb(200, 0, 0)';
-			ctx.fillRect(x, 40, 50, 50);
+			ctx.fillRect(100, 100, 50, 50); // Relative to the size of the canvas. 
 
 			ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
 			ctx.fillRect(30, 30, 50, 50);
-		}
+		// }
     }
 
 	return (
