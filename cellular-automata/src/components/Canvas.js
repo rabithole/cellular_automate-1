986420e78a0936c3fedbf37f863410374e94d3a5
Robert Kulp <pedalinon@gmail.com>
2020-08-20 20:38:03 -0700

    working the square problem

--- cellular-automata/src/components/Canvas.js
+++ cellular-automata/src/components/Canvas.js
@@ -11,11 +11,13 @@ function Canvas(props) {
 
 		const canvas = document.getElementById('canvas');
 		console.log(canvas)
+
 		if (canvas.getContext) {
+			x = 100
 			const ctx = canvas.getContext('2d');
 
 			ctx.fillStyle = 'rgb(200, 0, 0)';
-			ctx.fillRect(10, 10, 50, 50);
+			ctx.fillRect(x, 40, 50, 50);
 
 			ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
 			ctx.fillRect(30, 30, 50, 50);
